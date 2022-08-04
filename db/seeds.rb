a04287aa3c088e0195417c16b819504c0cccf37f
Brent Barnes <BrenttBarness@gmail.com>
2022-08-04 09:53:24 -0400

    added randomly generated seed data for flight model

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,28 +26,36 @@ Airport.create!([{
   location: "Denver, CO"
 }])
 
-Flight.create! ([{
-  departure_airport_id: 1,
-  arrival_airport_id: 2,
-  start_datetime: Date.today,
-  flight_duration: "1hr 53m"
-},
-{
-  departure_airport_id: 1,
-  arrival_airport_id: 3,
-  start_datetime: Date.today,
-  flight_duration: "4hr 21m"
-},
-{
-  departure_airport_id: 2,
-  arrival_airport_id: 1,
-  start_datetime: Date.today,
-  flight_duration: "4hr 21m"
-},
-{
-  departure_airport_id: 3,
-  arrival_airport_id: 1,
-  start_datetime: Date.today,
-  flight_duration: "4hr 21m"
-}
-])
+50.times do
+  Flight.create(departure_airport_id: Faker::Number.within(range: 1..8),
+                arrival_airport_id: Faker::Number.within(range: 1..8),
+                start_datetime: Faker::Date.in_date_period(year: 2022, month: 8),
+                flight_duration: "#{rand(1..5)} hour and #{rand(0..59)} minute(s)")
+end
+
+
+# Flight.create! ([{
+#   departure_airport_id: 1,
+#   arrival_airport_id: 2,
+#   start_datetime: Date.today,
+#   flight_duration: "1hr 53m"
+# },
+# {
+#   departure_airport_id: 1,
+#   arrival_airport_id: 3,
+#   start_datetime: Date.today,
+#   flight_duration: "4hr 21m"
+# },
+# {
+#   departure_airport_id: 2,
+#   arrival_airport_id: 1,
+#   start_datetime: Date.today,
+#   flight_duration: "4hr 21m"
+# },
+# {
+#   departure_airport_id: 3,
+#   arrival_airport_id: 1,
+#   start_datetime: Date.today,
+#   flight_duration: "4hr 21m"
+# }
+# ])
