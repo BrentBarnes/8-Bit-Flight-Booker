f3ac4f8f4732511df450abcaacfdafb681965768
Brent Barnes <BrenttBarness@gmail.com>
2022-08-04 18:28:50 -0400

    flight seed progress - every airport is connected to every other airport

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,13 +26,66 @@ Airport.create!([{
   location: "Denver, CO"
 }])
 
-Flight.destroy_all
 
-50.times do
-  Flight.create(departure_airport_id: Faker::Number.within(range: 1..8),
-                arrival_airport_id: Faker::Number.within(range: 1..8),
-                flight_number: Faker::Number.number(digits: 5),
-                departure_date: Faker::Date.in_date_period(year: 2022, month: 8),
-                departure_time: Faker::Time.between(from: DateTime.now, to: DateTime.tomorrow, format: :short).last(5),
-                flight_duration: "#{rand(1..5)} hour and #{rand(0..59)} minute(s)")
+# 20.times do
+#   Flight.create(departure_airport_id: Faker::Number.within(range: 1..8),
+#                 arrival_airport_id: Faker::Number.within(range: 1..8),
+#                 flight_number: Faker::Number.number(digits: 5),
+#                 departure_date: Faker::Date.in_date_period(year: 2022, month: 8),
+#                 departure_time: Faker::Time.between(from: DateTime.now, to: DateTime.tomorrow, format: :short).last(5),
+#                 flight_duration: "#{rand(1..5)} hour and #{rand(0..59)} minute(s)")
+# end
+
+
+Airport.find_each do |airport|
+  i = 1
+  while i <= 8
+    if airport.id == i
+      i += 1
+    else
+      Flight.create(departure_airport_id: airport.id,
+                    arrival_airport_id: i,
+                    flight_number: Faker::Number.number(digits: 5),
+                    departure_date: Faker::Date.in_date_period(year: 2022, month: 8),
+                    departure_time: Faker::Time.between(from: DateTime.now, to: DateTime.tomorrow, format: :short).last(5),
+                    flight_duration: "#{rand(1..5)} hour and #{rand(0..59)} minute(s)")
+      i += 1
+    end
+  end
+  i = 1
 end
+
+
+# Flight.create! ([{
+#   departure_airport_id: 1,
+#   arrival_airport_id: 2,
+#   flight_number: Faker::Number.number(digits: 5),
+#   departure_date: Faker::Date.in_date_period(year: 2022, month: 8),
+#   departure_time: Faker::Time.between(from: DateTime.now, to: DateTime.tomorrow, format: :short).last(5),
+#   flight_duration: "#{rand(1..5)} hour and #{rand(0..59)} minute(s)"
+# },
+# {
+#   departure_airport_id: 1,
+#   arrival_airport_id: 3,
+#   flight_number: Faker::Number.number(digits: 5),
+#   departure_date: Faker::Date.in_date_period(year: 2022, month: 8),
+#   departure_time: Faker::Time.between(from: DateTime.now, to: DateTime.tomorrow, format: :short).last(5),
+#   flight_duration: "#{rand(1..5)} hour and #{rand(0..59)} minute(s)"
+# },
+# {
+#   departure_airport_id: 2,
+#   arrival_airport_id: 1,
+#   flight_number: Faker::Number.number(digits: 5),
+#   departure_date: Faker::Date.in_date_period(year: 2022, month: 8),
+#   departure_time: Faker::Time.between(from: DateTime.now, to: DateTime.tomorrow, format: :short).last(5),
+#   flight_duration: "#{rand(1..5)} hour and #{rand(0..59)} minute(s)"
+# },
+# {
+#   departure_airport_id: 3,
+#   arrival_airport_id: 1,
+#   flight_number: Faker::Number.number(digits: 5),
+#   departure_date: Faker::Date.in_date_period(year: 2022, month: 8),
+#   departure_time: Faker::Time.between(from: DateTime.now, to: DateTime.tomorrow, format: :short).last(5),
+#   flight_duration: "#{rand(1..5)} hour and #{rand(0..59)} minute(s)"
+# }
+# ])
