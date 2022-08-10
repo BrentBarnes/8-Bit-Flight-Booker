57942c182cf64b1a051b10cfc6c30a6b6b9a1cdd
Brent Barnes <BrenttBarness@gmail.com>
2022-08-10 16:57:39 -0400

    added matching flights to index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,7 @@ class FlightsController < ApplicationController
     @airport_options = Airport.all.map { |a| [a.location, a.id] }
     @date_options = (Date.new(2022, 8, 1)..Date.new(2022, 8, -1)).map { |d| [d.strftime("%a, %b %d"), d] }
     @passenger_options = (1..4).map { |p| [p.to_s, p]}
+
+    @flights = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND departure_date = ?", params[:departure_airport_id], params[:arrival_airport_id], params[:departure_date])
   end
 end
