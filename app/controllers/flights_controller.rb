0fbba78321c8e92469e11cb708f3e6d1f8ff42a3
Brent Barnes <BrenttBarness@gmail.com>
2022-08-10 14:47:30 -0400

    updated departure date dropdown format

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| [a.location, a.id] }
-    @date_options = (Date.new(2022, 8, 1)..Date.new(2022, 8, -1)).map { |d| [d, d] }
+    @date_options = (Date.new(2022, 8, 1)..Date.new(2022, 8, -1)).map { |d| [d.strftime("%a, %b %d"), d] }
     @passenger_options = (1..4).map { |p| [p.to_s, p]}
   end
 end
